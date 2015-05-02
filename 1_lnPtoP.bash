bbd4b770f362a47ba00277f40366f0a98688bfa1
U-HNISHIT100TA\Hiroshi <Hiroshi@hnishiT100TA.(none)>
2015-05-03 00:15:57 +0900

    1_com

--- 1_lnPtoP.bash
+++ 1_lnPtoP.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-g++ lnPtoP.cpp -o lnPtoP.exe > res1.log
+g++ lnPtoP.cpp -o lnPtoP.exe  
 
-./lnPtoP.exe P_E_T300.dat Pc_T300.dat #$1:ln(P(E,300)), $2:output name
+./lnPtoP.exe P_E_T300.dat Pc_T300.dat > res1.log #$1:ln(P(E,300)), $2:output name
