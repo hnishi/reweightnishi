93832f918a8b2a1f54ef80e87c46a8a960fc3436
U-HNISHIT100TA\Hiroshi <Hiroshi@hnishiT100TA.(none)>
2015-05-02 10:13:56 +0900

    2_com 3_com

--- 2_exPotEne.bash
+++ 2_exPotEne.bash
@@ -5,3 +5,7 @@ g++ exPotEne.cpp tranishi.cpp pdbnishi.cpp inpnishi.cpp -o exPotEne.exe
 
 ./exPotEne.exe inp_exPotEne.inp
 
+cat c1c2c3c4str.dat | awk '{print $1 "   " $2}' > aaa
+paste aaa out_exPotEne.dat > file_3.dat
+
+rm aaa
