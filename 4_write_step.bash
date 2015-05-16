ad780b9772689ab5fd08f90a692a333679e1ae90
U-HNISHIT100TA\Hiroshi <Hiroshi@hnishiT100TA.(none)>
2015-05-16 16:49:17 +0900

    change to tee

--- 4_write_step.bash
+++ 4_write_step.bash
@@ -14,22 +14,23 @@ dir=`pwd`
 iend=`wc -l $file | cut -d' ' -f 1`
 mkdir $dir/list_stru
 
-echo $iend
+echo total number of structures = $iend
 
 #echo 74 > aaa.inp
 #rm aaa.inp
  
 for (( i=1; i<=$iend; i++ ))
 do
-   tail -$i $file > aaa
-   num=`head -1 aaa` 
-   echo $num > bbb
-   echo $num 
+   #tail -$i $file > aaa
+   #num=`head -1 aaa` 
+   #echo $num > bbb
+   #echo $num 
+   tail -$i $file | head -1 | tee bbb  # "tee" command do both echo and make file
    $ex_writestep mdx.crd mdx.in.pdb < bbb > bbb.log
    mv out_step.pdb list_stru/$num.pdb
 done
 
-rm aaa bbb bbb.log
+rm bbb bbb.log
 
 echo end
 #ls $dir/list_stru
